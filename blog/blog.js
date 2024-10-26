dcd126f00f71c9d15e4e2dd52416e0df85e8fd71
David Gosney <david.aphy@gmail.com>
2024-10-26 17:50:35 -0600

    blog 2

--- blog/blog.js
+++ blog/blog.js
@@ -23,5 +23,61 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
-	}
-]
+	},
+    {
+        id: 3,
+        title: "Belgariad Book One: Pawn of Prophecy",
+        date: "Feb 12, 2022",
+        description:
+        'A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his "Aunt Pol" and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.',
+        imgSrc:
+        "https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+        imgAlt: "Book cover for Pawn of Prophecy",
+        ages: "12-16",
+        genre: "Fantasy",
+        stars: "⭐⭐⭐⭐⭐"
+    }
+];
+
+const mainContent = document.querySelector(".main-content")
+
+function displayArticles() {
+	articles.forEach(item => {
+		const newDiv1 = document.createElement("div")
+		newDiv1.classList.add("book-info")
+
+		newDiv1.innerHTML = `
+			<div>
+				<h2>${item.date}</h2>
+				<h3>${item.ages}</h3>
+				<h3>${item.genre}</h3>
+				<p>${item.stars}</p>
+			</div>`
+
+		mainContent.appendChild(newDiv1)
+		
+		const newDiv2 = document.createElement("div")
+		newDiv2.classList.add("book")
+
+		newDiv2.innerHTML = `
+			<div>    
+				<h2>${item.title}</h2>
+				<img src="${item.imgSrc}" alt="${item.imgAlt}" width="250px">
+				<p>${item.description}</p>
+			</div>`
+
+		mainContent.appendChild(newDiv2)
+		
+		const newDiv3 = document.createElement("div")
+		newDiv3.classList.add("filter")
+
+		newDiv3.innerHTML = `
+			<div>
+                <p></p>
+            </div>`
+
+		mainContent.appendChild(newDiv3)
+	})
+}
+
+displayArticles()
